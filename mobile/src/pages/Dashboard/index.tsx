a254db9e43e7d2cf97c118a8f92ad66bad1e1bac
hugorodrigues13 <franciscohugo0@gmail.com>
2024-01-30 23:12:39 -0300

    tela order

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,18 +1,92 @@
-import React, {useContext} from "react"
-import { View, Text, Button } from "react-native"
+import React, {useContext, useState} from "react"
+import { View, Text, SafeAreaView, TouchableOpacity, TextInput, StyleSheet } from "react-native"
 
 import { AuthContext } from "../../contexts/AuthContext"
 
+import { useNavigation } from "@react-navigation/native"
+
+import { NativeStackNavigationProp } from "@react-navigation/native-stack"
+import { StackPramsList } from "../../routes/app.routes"
+
 export default function Dashboard(){
+    const navigation = useNavigation<NativeStackNavigationProp<StackPramsList>>();
     const {signOut} = useContext(AuthContext)
 
+    const [number, setNumber] = useState('');
+
+    async function openOrder(){
+        if(number === ''){
+            return;
+        }
+
+        //precisa fazer a requisição e abrir a mesa e navegar para proxima tela
+        navigation.navigate('Order', { number: number, order_id: ''})
+    }
+
+
     return(
-        <View>
-            <Text>Tela Dashboard</Text>
-            <Button
-                title="Sair do app"
-                onPress={signOut}
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Novo pedido</Text>
+
+            <TextInput
+                placeholder="Numero da mesa"
+                placeholderTextColor="#F0F0F0"
+                style={styles.input}
+                keyboardType='numeric'
+                value={number}
+                onChangeText={setNumber}
             />
-        </View>
+
+            <TouchableOpacity style={styles.button} onPress={openOrder}>
+                <Text style={styles.buttonText}>Abrir Mesa</Text>
+            </TouchableOpacity>
+
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 15,
+        backgroundColor: '#1d1d2e'
+    },
+
+    title:{
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 24,
+
+    },
+
+    input: {
+        width: '90%',
+        height: 60,
+        backgroundColor: '#101026',
+        paddingHorizontal: 8,
+        borderRadius: 4,
+        textAlign: 'center',
+        fontSize: 22,
+        color: '#FFF'
+    },
+
+    button:{
+        width: '90%',
+        height: 40,
+        backgroundColor: '#3fffa3',
+        borderRadius: 4,
+        marginVertical: 12,
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+
+    buttonText:{
+        fontSize: 18,
+        color: '#101026',
+        fontWeight: 'bold',
+    },
+})
---
+++ mobile/src/pages/Order/index.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
+
+import {useRoute, RouteProp} from '@react-navigation/native'
+
+import {Feather} from '@expo/vector-icons'
+
+type RouteDetailParams = {
+    Order: {
+        number: string | number;
+        order_id: string;
+    }
+}
+
+type OrderRouteProps = RouteProp<RouteDetailParams, 'Order'>
+
+export default function Order(){
+    const route = useRoute<OrderRouteProps>()
+
+    return(
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Mesa {route.params.number}</Text>
+                <TouchableOpacity>
+                    <Feather name="trash-2" size={28} color="#ff3f4b"/>
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{color: '#fff'}}>Pizzas</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.input}>
+                <Text style={{color: '#fff'}}>Pizzas de calabresa</Text>
+            </TouchableOpacity>
+
+            <View style={styles.qtdContainer}>
+                <Text style={styles.qtdText}>Quantidade</Text>
+                <TextInput 
+                    style={[styles.input, {width: '60%', textAlign: 'center'}]}
+                    placeholderTextColor="#fff"
+                    keyboardType='numeric'
+                    value='1'
+                />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingVertical: '5%',
+        paddingEnd: '4%',
+        paddingStart: '4%',
+        backgroundColor: '#1d1d2e',
+    },
+
+    header: {
+        flexDirection: 'row',
+        marginBottom: 16,
+        alignItems: 'center',
+        marginTop: 24
+    },
+
+    title: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginRight: 14
+    },
+    input: {
+        backgroundColor: '#101026',
+        borderRadius: 4,
+        width: '100%',
+        height: 40,
+        marginBottom: 12,
+        justifyContent: 'center',
+        paddingHorizontal: 8,
+        color: '#fff',
+        fontSize: 20
+    },
+
+    qtdContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+
+    qtdText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#fff'
+    }
+})
