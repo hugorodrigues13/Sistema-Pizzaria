5f2450784d6d5c5e1da2964923b59e7b0baebf5c
hugorodrigues13 <franciscohugo0@gmail.com>
2024-04-09 22:49:58 -0300

    concluido

--- client/src/components/ModalOrder/index.tsx
+++ client/src/components/ModalOrder/index.tsx
@@ -1,65 +1,57 @@
 import Modal from 'react-modal'
-import styles from './style.module.scss'
 import { Container, HeaderContent, Content, ListContent, ButtonFooter } from './styles'
-
 import {FiX} from 'react-icons/fi'
-
 import {OrderItemProps} from '@/pages/dashboard'
-import { TextArea } from '../ui/Input'
+import { useState } from 'react';
 
 interface ModalOrderProps{
     isOpen: boolean;
     onRequestClose: () => void;
     order?: OrderItemProps[] ;
     handleFinishOrder: (id: string) => void;
+    handleCancelOrder: (id: string) => void;
 }
 
-export function ModalOrder({ isOpen, onRequestClose, order, handleFinishOrder}: ModalOrderProps){
+export function ModalOrder({ isOpen, onRequestClose, order, handleFinishOrder, handleCancelOrder }: ModalOrderProps) {
+    const [orderId, setOrderId] = useState('');
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
-     // Tratando a possibilidade de order ser undefined
-  const tableNumber = order ? (order[0]?.order?.table ?? 'N/A') : 'N/A';
-  const orderId = order ? (order[0]?.order_id ?? 'N/A') : 'N/A';
+    const tableNumber = order ? (order[0]?.order?.table ?? 'N/A') : 'N/A';
+    const orderItem = order ? (order[0]?.order_id ?? 'N/A') : 'N/A';
 
-    const customStyles = {
-        content: {
-            top: '50%',
-            bottom: 'auto',
-            left: '50%',
-            right: 'auto',
-            padding: '30px',
-            transform: 'translate(-50%, -50%)',
-            backgroundColor: '#003249',
-        },
-        overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)' // Aqui você pode definir a cor de fundo desejada, 0.5 é a opacidade
-          }
+    const handleCancel = () => {
+        // Define o orderItem para usar dentro da função confirmCancelOrder
+        setOrderId(orderItem);
+        setShowConfirmation(true);
     }
 
-      // Agrupar itens por nome e calcular a quantidade total de cada item
-      const groupedItems = order?.reduce((acc: {[key: string]: OrderItemProps}, currentItem: OrderItemProps) => {
-        if (!acc[currentItem.product.name]) {
-            acc[currentItem.product.name] = { ...currentItem };
-        } else {
-            acc[currentItem.product.name].amount += currentItem.amount;
-        }
-        return acc;
-        
-    }, {});
 
-    return(
+    return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
-            style={customStyles}
+            style={{
+                content: {
+                    top: '50%',
+                    bottom: 'auto',
+                    left: '50%',
+                    right: 'auto',
+                    padding: '30px',
+                    transform: 'translate(-50%, -50%)',
+                    backgroundColor: '#003249',
+                },
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }}
         >
-
             <Container>
                 <HeaderContent>
                     <button
                         type='button'
                         onClick={onRequestClose}
                         className="buttonClose"
-                        style={{background: 'transparent', border: 0}}
+                        style={{ background: 'transparent', border: 0 }}
                     >
                         <FiX size={45} color='#f34748' />
                     </button>
@@ -69,38 +61,50 @@ export function ModalOrder({ isOpen, onRequestClose, order, handleFinishOrder}: 
                 </HeaderContent>
 
                 <Content>
-                    <p>Detalhes do pedido</p>
-                    <ListContent>
-                        {groupedItems && Object.values(groupedItems).map(item => (
-                            
-                                <section key={item.id} className="containerItem">
-                                        <span><strong>{item.amount}</strong> - {item.product.name}</span>
-                                </section>
-                                
-                            ))}
-                            
-                    </ListContent>
-                    
-                    {order && order[0] && order[0].order.name !== null ? (
-                        <div className='scroll-container'>
-                            <span>{order[0].order.name}</span>
+                    {showConfirmation ? (
+                        <div>
+                            <p>Tem certeza que deseja cancelar este pedido?</p>
+                            <ButtonFooter>
+                                <button className="buttonOrder" onClick={() => handleCancelOrder(orderId)}>
+                                    Confirmar
+                                </button>
+                                <button className="buttonCancel" onClick={() => setShowConfirmation(false)}>
+                                    Cancelar
+                                </button>
+                            </ButtonFooter>
                         </div>
-                        ) : (
-                            <span>Sem observação..</span>
+                    ) : (
+                        <>
+                            <p>Detalhes do pedido</p>
+                            <ListContent>
+                                {order && order.map(item => (
+                                    <section key={item.id} className="containerItem">
+                                        <span><strong>{item.amount}</strong> - {item.product.name}</span>
+                                    </section>
+                                ))}
+                            </ListContent>
+
+                            {/* {console.log(order)} */}
+                            {order && order[0] && (order[0].order.name !== null && order[0].order.name !== "") ? (
+                                <div className='scroll-container'>
+                                    <span>{order[0].order.name}</span>
+                                </div>
+                            ) : (
+                                <span>Sem observação..</span>
+                            )}
+
+                            <ButtonFooter>
+                                <button className="buttonOrder" onClick={() => handleFinishOrder(orderItem)}>
+                                    Concluir
+                                </button>
+                                <button className="buttonCancel" onClick={handleCancel}>
+                                    Cancelar pedido
+                                </button>
+                            </ButtonFooter>
+                        </>
                     )}
-
-
-                    <ButtonFooter>
-                        <button className="buttonOrder" onClick={ () => handleFinishOrder(orderId)}>
-                            Concluir
-                        </button>
-                        <button className="buttonCancel" onClick={ () => handleFinishOrder(orderId)}>
-                            Cancelar pedido
-                        </button>
-                    </ButtonFooter>
                 </Content>
             </Container>
-
         </Modal>
     )
 }
