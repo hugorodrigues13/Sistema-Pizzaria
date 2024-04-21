a51c0f988ce52a44c72eed95a3703d3666fb0519
hugorodrigues13 <franciscohugo0@gmail.com>
2024-04-21 11:51:42 -0300

    Lista de categorias concluída.

--- client/src/components/ListContainer/styles.ts
+++ client/src/components/ListContainer/styles.ts
@@ -2,21 +2,24 @@ import styled from "styled-components";
 
 export const Container = styled.main`
     width: 100%;
-    background-color: ${props => props.theme.colors.white};
     margin: 0 auto;
 
 `;
 
 export const Table = styled.table`
     width: 100%;
+    background-color: ${props => props.theme.colors.white};
     border-collapse: collapse;
 
-    @media screen and (max-width: 700px) {
+ @media screen and (max-width: 700px) {
         td {
             font-size: 0.8rem; /* Reduzindo o tamanho da fonte */
         }
 
-}
+        button{
+            font-size: 0.8rem;
+        }
+    }
 `;
 
 export const Thead = styled.thead`
@@ -45,8 +48,7 @@ export const DataRow = styled.tr`
     }
 `;
 
-export const Cell = styled.td`
-`;
+export const Cell = styled.td``;
 
 export const Pagination = styled.div`
     display: flex;
@@ -54,6 +56,7 @@ export const Pagination = styled.div`
     justify-content: right;
     padding: 1rem;
     color: ${props => props.theme.colors.primary};
+    background-color: ${props => props.theme.colors.white};
     font-weight: bold;
 `;
 
@@ -98,6 +101,22 @@ export const LeftAlignedCell = styled(Cell)`
     text-align: left;
     padding: 0 1rem;
     width: 60%;
+
+    button {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: transparent;
+        border: none;
+        color: ${props => props.theme.colors.gray};
+        font-weight: bold;
+
+        div {
+            display: flex;
+            margin-left: 0.5rem;
+            flex-direction: column;
+        };
+    }
 `;
 
 // Componente para alinhar o conteúdo das células à direita e ocupar 20% do width
@@ -106,11 +125,31 @@ export const RightAlignedCell = styled(Cell)`
     border-left: 1px solid #fff; /* Adiciona uma borda esquerda em todas as células */
     width: 10%;
 
-    >span {
-        display: inline-block;
-        width: 2rem; /* Definindo um tamanho fixo para ambos os textos */
-        text-align: center;
+    button {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        background-color: transparent;
+        border: none;
+        color: ${props => props.theme.colors.gray};
         font-weight: bold;
+
+        div {
+            display: flex;
+            margin-left: 0.5rem;
+            flex-direction: column;
+        };
+    }
+
+    .sim {
+        font-size: 1.1rem;
+        color: ${(props) => props.theme.colors.primary}; /* Estilo para "SIM" */
+    }
+
+    .nao {
+        font-size: 1.0rem;
+        color: red; /* Estilo para "NAO" */
     }
 
     >svg {
@@ -121,4 +160,97 @@ export const RightAlignedCell = styled(Cell)`
             transform: scale3d(1.2, 1.2, 1)
         }
     }
+`;
+
+export const ModalDelete = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .confirmation-modal {
+        width: 30%;
+        min-width: 300px;
+        background-color: ${props => props.theme.colors.primary};
+        height: 120px;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        display: flex;
+        flex-direction: column;
+    }
+
+    .confirmation-modal p {
+        color: ${props => props.theme.colors.white};
+        margin-bottom: 10px;
+    }
+
+    .button-div  {
+        display: flex;
+        justify-content: space-between;
+        margin-top: 20px;
+
+        button {
+            width: 30%;
+            height: 30px;
+            border: none;
+            border-radius: 0.5rem;
+            color: ${props => props.theme.colors.primary};
+            margin-top: auto; /* Faz o botão ficar na parte inferior */
+            transition: transform 0.5s;
+
+            &:hover{
+                transform: scale(1.1);
+            }
+        }
+    }
+
+    .button-confirm {
+        background-color: ${props => props.theme.colors.success};
+    }
+
+    .button-cancel {
+        background-color: ${props => props.theme.colors.info};
+    }
+`;
+
+export const SearchFilter = styled.div`
+    margin-bottom: 2rem;
+
+    .content-search {
+        display: flex;
+        height: 40px;
+        width: 30%;
+        justify-content: space-between;
+        background-color: ${props => props.theme.colors.primary};
+        border: solid 1px ${props => props.theme.colors.gray};
+
+        input{
+            width: 100%;
+            padding: 0 0.5rem;
+            color: ${props => props.theme.colors.gray};
+            background-color: transparent;
+            border: none;
+        }
+
+        input::placeholder {
+            color: ${props => props.theme.colors.gray};
+        }
+
+        >svg {
+            display: flex;
+            height: 40px;
+            margin-right: 0.5rem;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            color: ${props => props.theme.colors.gray};
+        }
+    }
+    
 `;
--- client/src/pages/category/[list]/index.tsx
+++ client/src/pages/category/[list]/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from './styles';
 import Head from 'next/head';
 import { Header } from '@/components/Header';
@@ -6,6 +6,7 @@ import { canSSRAuth } from '@/utils/canSSRAuth';
 
 import { ListContainer } from '@/components/ListContainer';
 import { setupAPIClient } from '@/services/api';
+import { toast } from 'react-toastify';
 
 interface Category {
     id: string;
@@ -25,10 +26,53 @@ interface ListCategoryProps {
 
 
 const ListCategory = ({categories, products}: ListCategoryProps) => {
+    const [sortedItems, setSortedItems] = useState([...(categories || [])]);
+
     const getCategoryProductStatus = (categoryId: string) => {
         // Verifica se algum produto possui o category_id correspondente à categoria
         return products.some(product => product.category_id === categoryId) ? "SIM" : "NAO";
     };
+
+    // Sua função que traz a listagem e ordena os itens
+    const getSortedItems = async () => {
+        try {
+          const apiClient = setupAPIClient();
+          const response = await apiClient.get('/category');
+          const sortedItems = response.data; // Assumindo que a API já retorna a lista ordenada
+          return sortedItems;
+
+        } catch (error) {
+          return [];
+        }
+      };
+    
+      const fetchOrders = async () => {
+        const sortedItems = await getSortedItems();
+        setSortedItems(sortedItems);
+      };
+
+    const handleDeleteCategory = async (id: string) => {
+        const status = getCategoryProductStatus(id);
+        if (status === "SIM") {
+          // Se houver produtos vinculados, exiba uma mensagem de erro
+          toast.error("Não é possível excluir categorias com produtos vinculados.");
+        } else {
+          try {
+            // Faça a solicitação de exclusão da categoria para o servidor
+            const apiClient = setupAPIClient();
+            await apiClient.delete(`/category/remove?category_id=${id}`);
+      
+            // Se a exclusão for bem-sucedida, atualize a lista de categorias
+            toast.success("Categoria excluída com sucesso!");
+      
+            await fetchOrders();
+            
+          } catch (error) {
+            // Se ocorrer um erro durante a exclusão, exiba uma mensagem de erro
+            toast.error("Ocorreu um erro ao excluir a categoria.");
+          }
+        }
+      };
 
     return (
         <>
@@ -40,8 +84,10 @@ const ListCategory = ({categories, products}: ListCategoryProps) => {
              <Header/>
                 <Container>
                     <ListContainer
+                        handleDeleteCategory={handleDeleteCategory}
                         getCategoryProductStatus={getCategoryProductStatus}
-                        data={categories}
+                        data={sortedItems}
+                        children="PRODUTOS"
                     />
                     
                 </Container>
